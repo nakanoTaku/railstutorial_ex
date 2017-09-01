dae5bea8c5e9bf9d3746eb194e7e5b94ce6e8615
taku <kosmo.waizu0804@gmail.com>
2017-09-01 19:41:18 +0900

    ユーザー一覧にて検索はできるようになった()

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
+  post '/serach', to: 'users#serach'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users do
