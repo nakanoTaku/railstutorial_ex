582d8c551ec2fb0d81bdd0798d7c283ed5236a58
taku <kosmo.waizu0804@gmail.com>
2017-09-08 17:29:34 +0900

    いいねボタンをajaxで処理できるようにした（がアイコンが消えちゃう。）

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,23 @@
 class LikesController < ApplicationController
   def create
     micropost = Micropost.find(params[:micropost_id])
-
     Like.create(user_id: micropost.user_id, micropost_id: micropost.id)
-    redirect_to root_path
+    @micropost = Micropost.find(params[:micropost_id])
+
+    respond_to do |format|
+      format.html { redirect_to root_path}
+      format.js
+    end
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
-
     Like.find_by(user_id: micropost.user_id, micropost_id: micropost.id).destroy
-    redirect_to root_path
+    @micropost = Micropost.find(params[:micropost_id])
+
+    respond_to do |format|
+      format.html { redirect_to root_path}
+      format.js
+    end
   end
 end
